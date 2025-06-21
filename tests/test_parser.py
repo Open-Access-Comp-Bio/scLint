0754df398ab68f538d8213b23942881f63ef66c0
Tanvir Saini <tanvirsaini@Tanvirs-MacBook-Pro-2.local>
2025-06-20 17:04:22 -0700

    Pytest housekeeping. Moved example data for pytest into collateral folder. Still need to downsize the sample numbers from 58k. Next commit it will be taken care of. Added pytest fixture.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,44 +1,32 @@
 import pytest
 #temporary line to be replaced once the package is properally installed
-from parser_answers import ROOT_DICT, pool_files_ans, id_files_ans
+from tests.parser_answers import ROOT_DIR, pool_files_ans, id_files_ans
 import sys
-sys.path.insert(1, '/Users/tanvirsaini/Documents/oacb/scLint/src/scLint')
+sys.path.insert(1, '/Users/tanvirsaini/Documents/oacb/scLint/src/scLint/')
 from sc_parser_utility import pool_files, id_files, open_files, create_anndata
-import logging_messages
 
+@pytest.fixture
+def pooled_files():
+    return pool_files_ans
 
-def test_pool_files():
-    results =  pool_files(ROOT_DICT)
-    assert results == pool_files_ans
+@pytest.fixture
+def identified_files():
+    return id_files_ans
 
-def test_id_files():
-    files = pool_files_ans
-    identified_files = id_files(files)
-    assert identified_files == id_files_ans
+@pytest.fixture
+def sorted_files():
+    return 
 
-# TODO Tuesday's goal create a smaller test file
-# TODO model pytest after Steve's work
-# TODO adjust logging messages to match Steve's works
+def test_pool_files(pooled_files):
+    results =  pool_files(ROOT_DIR)
+    assert results == pooled_files
+
+def test_id_files(pooled_files, identified_files):
+    id_files_ans = id_files(pooled_files)
+    assert id_files_ans == identified_files
 
 def test_open_files():
-    results = open_files(id_files_ans)
-    return(results)
+    """TODO"""
 
-def test_anndata(results):
-    my_adata = create_anndata(results,False, '1strun')
-    return my_adata
-
-files = pool_files(ROOT_DICT)
-print(files)
-myids = id_files(files)
-print(myids)
-
-logging_messages.activate()
-results = test_open_files()
-# import pickle
-# with open('dataframes.pkl', 'wb') as f:
-#     pickle.dump(results, f)
-output = test_anndata(results)
-print(output)
-print(output.X)
-print(output.layers)
+def test_anndata():
+    """TODO"""
