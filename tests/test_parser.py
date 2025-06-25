c1c4a69f127afbcb4db0895e9ca33f97e373c7ee
Tanvir Saini <tanvirsaini@Tanvirs-MacBook-Pro-2.local>
2025-06-25 10:35:40 -0700

    Finalized pytests for scParser. Added in structure validation (test_open_files) and ensuring the AnnData Object is properly created (test_anndata)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,14 @@
 import pytest
-#temporary line to be replaced once the package is properally installed
-from tests.parser_answers import ROOT_DIR, pool_files_ans, id_files_ans, sorted_files_ans, myanndata
+import pandas as pd
+import scipy.sparse as sp
+import anndata as ad
+from tests.parser_answers import ROOT_DIR, pool_files_ans, id_files_ans
+### NOTE temporary line to be replaced once the package is properally installed
 import sys
 sys.path.insert(1, '/Users/tanvirsaini/Documents/oacb/scLint/src/scLint/')
+### END OF NOTE
 from sc_parser_utility import pool_files, id_files, open_files, create_anndata
+
 
 @pytest.fixture
 def pooled_files():
@@ -13,33 +18,24 @@ def pooled_files():
 def identified_files():
     return id_files_ans
 
-@pytest.fixture
-def sorted_files():
-    return sorted_files_ans
-
-@pytest.fixture
-def my_anndata():
-    return myanndata
-
 def test_pool_files(pooled_files):
     results =  pool_files(ROOT_DIR)
     assert results == pooled_files
 
 def test_id_files(pooled_files, identified_files):
-    id_files_ans = id_files(pooled_files)
-    assert id_files_ans == identified_files
+    id_files_output = id_files(pooled_files)
+    assert id_files_output == identified_files
 
-def test_open_files():
-    # TODO MAKE SURE TO MAKE SMALLER FILES FIRST
-    # THEN YOU CAN OUTPUT THOSE TO FILES TO COMPARE
-    # OR TRY SOMETHING ELSE
-    """TODO"""
+def test_open_files(identified_files):
+    result = open_files(identified_files)
+    assert isinstance(result, dict)
+    for key, value in result.items():
+        if key == 'uns':
+            assert isinstance(value, list)
+        else:
+            assert isinstance(value, (pd.DataFrame, sp.spmatrix))
 
-def test_anndata():
-    # TODO ASSERT THAT THE NEWLY GENERATED ANNDATA
-    # MATCHES THE STORED H5AD ANNDATA.
-    """TODO"""
-
-def test_pipeline(identified_files):
+def test_anndata(identified_files):
     opened_files = open_files(identified_files)
-    create_anndata(opened_files, True)
+    generated_adata = create_anndata(opened_files)
+    assert isinstance(generated_adata, ad.AnnData)
